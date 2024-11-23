e06971e3f094dcd2dd4546c8527347ab70acbe8e
guilherme <guilherme.souza.1997@outlook.com>
2024-11-23 15:27:02 -0300

    curso java commitado

---
+++ curso_programacao/src/exercicio.java
@@ -0,0 +1,20 @@
+public class exercicio {
+
+    public static void main(String[] args) {
+
+        String product1 = "computer";
+        String product2 = "office desk";
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+        double price1 = 2100.0;
+        double price2 = 650.0;
+        double measure = 53.234567;
+
+        System.out.println("products:");
+        System.out.printf("%s, which price is $ %.2f%n" +  "%s, which price is $ %.2f%n", product1, price1, product2, price2);
+        
+        System.out.printf("Record: " + "%d years old, code %d and gender: %s", age, code, gender);
+        
+    }
+}
